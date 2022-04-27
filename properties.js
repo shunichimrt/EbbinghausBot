33fd0bbe878900cbd3a73e6df53095432f979021
村田峻一 <shunichimurata@muratashunichinoMacBook-Pro.local>
2022-04-27 16:38:03 +0900

    エンドポイント削除

--- properties.js
+++ properties.js
@@ -1,7 +1,6 @@
 /** LINE Developers Info */
 var access_token =
   PropertiesService.getScriptProperties().getProperty("ACCESS_TOKEN");
-var line_endpoint = "https://api.line.me/v2/bot/message/reply";
 
 /** スプレッドシート */
 var SPREADSHEET_ID =
